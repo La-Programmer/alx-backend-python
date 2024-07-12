2ba56e1a64d5e8a26eda8f49fb2faa8e31be3ab8
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-12 12:54:24 +0100

    Fix task 1 again 4

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,13 +2,14 @@
 """Concurrent Coroutines"""
 
 import asyncio
+from typing import List
 wait_random: float = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> 'list[float]':
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Runs multiple coroutines concurrently"""
-    tasks: 'list[float]' = [wait_random(max_delay) for i in range(n)]
-    res: 'list[float]' = []
+    tasks: List[asyncio.Task] = [wait_random(max_delay) for i in range(n)]
+    res: List[float] = []
     for task in asyncio.as_completed(tasks):
         task: asyncio.Task
         result: float = await task
