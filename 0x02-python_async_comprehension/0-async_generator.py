44861982e352b4532863d3126b2691fbda39ce05
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-14 14:04:51 +0100

    Fix task 0 again 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,13 +3,13 @@
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[int, None]:
+async def async_generator() -> AsyncGenerator[int, None]:
     """Async generator function"""
     for i in range(10):
         i: int
         await asyncio.sleep(1)
-        random_result: Generator[int, None] = random.uniform(0, 10)
+        random_result: AsyncGenerator[int, None] = random.uniform(0, 10)
         yield random_result
